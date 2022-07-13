e650b733323b8603f66f69d093f81e21a73f08fa
Blancamc00 <blanca.manuc@gmail.com>
2022-07-13 10:03:14 +0100

    Add searches to book table.

---
+++ Bookish/Migrations/20220712174513_ModifyBooks6.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookish.Migrations
+{
+    public partial class ModifyBooks6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookCopies_Books_BookId",
+                table: "BookCopies");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumOfSearches",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BookId",
+                table: "BookCopies",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookCopies_Books_BookId",
+                table: "BookCopies",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookCopies_Books_BookId",
+                table: "BookCopies");
+
+            migrationBuilder.DropColumn(
+                name: "NumOfSearches",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BookId",
+                table: "BookCopies",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookCopies_Books_BookId",
+                table: "BookCopies",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Bookish/Migrations/20220713083831_ModifyBooks7.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookish.Migrations
+{
+    public partial class ModifyBooks7 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumOfSearches",
+                table: "Books",
+                newName: "Searches");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Searches",
+                table: "Books",
+                newName: "NumOfSearches");
+        }
+    }
+}
