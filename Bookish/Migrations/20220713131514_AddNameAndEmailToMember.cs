30c8138c9ebab7904283982e00412a0edaca932f
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-13 14:16:34 +0100

    Added Email and Name field Member Class.

---
+++ Bookish/Migrations/20220713131514_AddNameAndEmailToMember.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookish.Migrations
+{
+    public partial class AddNameAndEmailToMember : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Members");
+        }
+    }
+}
--- Bookish/Models/Member.cs
+++ Bookish/Models/Member.cs
@@ -4,4 +4,6 @@ public class Member
 {
     public string MemberId { get; set; }
     public virtual List<BookCopy> Books { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
 }
